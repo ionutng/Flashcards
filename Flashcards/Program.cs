7c592f559153002d42f7ebfc63eba08767df0d4c
ionutng <ionutngp@gmail.com>
2023-10-02 17:09:59 +0300

    Add project files.

---
+++ Flashcards/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+SqlConnection sqlConnection;
+
+string connectionString = @"Data Source=(LocalDb)\LocalDB;Initial Catalog=Flashcards;Integrated Security=True";
+
+try
+{
+    sqlConnection = new SqlConnection(connectionString);
+
+    sqlConnection.Open();
+
+    Console.WriteLine("Connected!");
+} 
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}

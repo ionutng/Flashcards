65d8f1201233c4e7b14bb729dc13b3da5aa636bd
ionutng <ionutngp@gmail.com>
2023-10-03 10:37:05 +0300

    Add CRUD operations for flashcards and add validation

--- Flashcards/DatabaseManager.cs
+++ Flashcards/DatabaseManager.cs
@@ -34,7 +34,7 @@ namespace Flashcards
                         ManageStacks();
                         break;
                     case "2":
-                        Console.WriteLine("\nManaging flashcards!");
+                        ManageFlashcards();
                         break;
                     case "0":
                         Environment.Exit(0);
@@ -89,8 +89,9 @@ namespace Flashcards
                 connection = new SqlConnection(connectionString);
                 connection.Open();
 
-                Console.WriteLine("\nPlease insert the name of the stack.");
-                string stackName = Console.ReadLine();
+                string stackName = Validation.GetString("Please insert the name of the stack or Type 0 to return to the main menu.");
+
+                CheckDuplicate(stackName);
 
                 string query = $"INSERT INTO Stacks (Name) VALUES ('"+ stackName +"')";
 
@@ -98,6 +99,7 @@ namespace Flashcards
 
                 command.ExecuteNonQuery();
 
+                Console.Clear();
                 Console.WriteLine($"Successfully added a {stackName} stack!");
 
                 connection.Close();
@@ -110,6 +112,7 @@ namespace Flashcards
 
         static void GetStacks()
         {
+            Console.Clear();
             SqlConnection connection;
 
             try
@@ -164,6 +167,8 @@ namespace Flashcards
                 }
                 else
                     Console.WriteLine("\nThere are no records yet!");
+
+                Console.WriteLine();
             }
             catch (Exception ex)
             {
@@ -176,8 +181,7 @@ namespace Flashcards
             GetStacks();
             SqlConnection connection;
 
-            Console.WriteLine("\nPlease type the id of the stack you would like to delete.");
-            string stackId = Console.ReadLine();
+            string stackId = Validation.GetString("Please type the id of the stack you would like to delete or Type 0 to return to the main menu.");
 
             try
             {
@@ -188,10 +192,11 @@ namespace Flashcards
 
                 SqlCommand command = new(query, connection);
 
+                Console.Clear();
                 if (command.ExecuteNonQuery() > 0)
-                    Console.WriteLine($"Stack #{stackId} successfully deleted!");
+                    Console.WriteLine($"Stack #{stackId} successfully deleted!\n");
                 else
-                    Console.WriteLine($"A stack with id: {stackId} doesn't exist!");
+                    Console.WriteLine($"A stack with id: {stackId} doesn't exist!\n");
 
                 connection.Close();
                 
@@ -207,25 +212,377 @@ namespace Flashcards
             GetStacks();
             SqlConnection connection;
 
-            Console.WriteLine("\nPlease type the id of the stack you would like to update.");
-            string stackId = Console.ReadLine();
+            string stackId = Validation.GetString("Please type the id of the stack you would like to update or Type 0 to return to the main menu.");
 
-            Console.WriteLine("\nPlease type the new name of the stack.");
-            string stackName = Console.ReadLine();
+            string stackName = Validation.GetString("Please type the new name of the stack or Type 0 to return to the main menu.");
 
             try
             {
                 connection = new SqlConnection(connectionString);
                 connection.Open();
 
-                string query = $"UPDATE Stacks SET Name = ('" + stackName + "') WHERE StackId = '"+ stackId + "'";
+                string query = $"UPDATE Stacks SET Name = ('" + stackName + "') WHERE StackId = '" + stackId + "'";
 
                 SqlCommand command = new(query, connection);
 
+                Console.Clear();
                 if (command.ExecuteNonQuery() > 0)
-                    Console.WriteLine($"Stack #{stackId} successfully updated!");
+                    Console.WriteLine($"Stack #{stackId} successfully updated!\n");
                 else
-                    Console.WriteLine($"A stack with id: {stackId} doesn't exist!");
+                    Console.WriteLine($"A stack with id: {stackId} doesn't exist!\n");
+
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void ManageFlashcards()
+        {
+            Console.Clear();
+            Console.WriteLine("| Flashcards Menu |\n");
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("\n--------------------------------------");
+            Console.WriteLine("Type A to Add a Flashcard.");
+            Console.WriteLine("Type V to View all Flashcard.");
+            Console.WriteLine("Type D to Delete a Flashcard.");
+            Console.WriteLine("Type U to Update a Flashcard.");
+            Console.WriteLine("--------------------------------------");
+
+            string flashcardOption = Console.ReadLine();
+
+            switch (flashcardOption.Trim().ToLower())
+            {
+                case "a":
+                    InsertFlashcard();
+                    break;
+                case "v":
+                    GetFlashcards();
+                    break;
+                case "d":
+                    DeleteFlashcard();
+                    break;
+                case "u":
+                    UpdateFlashcard();
+                    break;
+                default:
+                    Console.WriteLine("Not an available option. Please try again!");
+                    break;
+            }
+        }
+
+        static void InsertFlashcard()
+        {
+            SqlConnection connection;
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string flashcardQuestion = Validation.GetString("Please insert the question for the flashcard or Type 0 to return to the main menu.");
+                string flashcardAnswer = Validation.GetString("Please insert the correct answer for the question or Type 0 to return to the main menu.");
+
+                Console.WriteLine();
+                GetStacks();
+                string stackId = Validation.GetString("Please insert the id of the stack the flashcard should be in or Type 0 to return to the main menu.");
+
+                string query = $"INSERT INTO Flashcards (Question, Answer, StackId) VALUES ('" + flashcardQuestion + "', '" + flashcardAnswer + "', '" + stackId + "')";
+
+                SqlCommand command = new(query, connection);
+
+                command.ExecuteNonQuery();
+
+                Console.Clear();
+                Console.WriteLine($"Successfully added a flashcard!");
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void GetFlashcards()
+        {
+            Console.Clear();
+            SqlConnection connection;
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string query = $"SELECT FlashcardId, Question, Answer, Name FROM Flashcards " +
+                    $"INNER JOIN Stacks ON Flashcards.StackId = Stacks.StackId";
+
+                SqlCommand command = new(query, connection);
+
+                var tableData = new DataTable();
+                tableData.Load(command.ExecuteReader());
+
+                connection.Close();
+
+                if (tableData.Rows.Count > 0)
+                {
+                    ConsoleTableBuilder
+                        .From(tableData)
+                        .WithTitle("Flashcards", ConsoleColor.DarkBlue, ConsoleColor.DarkGray)
+                        .WithColumn("Id", "Question", "Answer", "Stack Name")
+                        .WithTextAlignment(new Dictionary<int, TextAligntment>
+                        {
+                            {0, TextAligntment.Center },
+                            {1, TextAligntment.Center },
+                            {2, TextAligntment.Center },
+                            {3, TextAligntment.Center },
+                        })
+                        .WithCharMapDefinition(new Dictionary<CharMapPositions, char> {
+                            {CharMapPositions.BottomLeft, '=' },
+                            {CharMapPositions.BottomCenter, '=' },
+                            {CharMapPositions.BottomRight, '=' },
+                            {CharMapPositions.BorderTop, '=' },
+                            {CharMapPositions.BorderBottom, '=' },
+                            {CharMapPositions.BorderLeft, '|' },
+                            {CharMapPositions.BorderRight, '|' },
+                            {CharMapPositions.DividerY, '|' },
+                        })
+                        .WithHeaderCharMapDefinition(new Dictionary<HeaderCharMapPositions, char> {
+                            {HeaderCharMapPositions.TopLeft, '=' },
+                            {HeaderCharMapPositions.TopCenter, '=' },
+                            {HeaderCharMapPositions.TopRight, '=' },
+                            {HeaderCharMapPositions.BottomLeft, '|' },
+                            {HeaderCharMapPositions.BottomCenter, '-' },
+                            {HeaderCharMapPositions.BottomRight, '|' },
+                            {HeaderCharMapPositions.Divider, '|' },
+                            {HeaderCharMapPositions.BorderTop, '=' },
+                            {HeaderCharMapPositions.BorderBottom, '-' },
+                            {HeaderCharMapPositions.BorderLeft, '|' },
+                            {HeaderCharMapPositions.BorderRight, '|' },
+                        })
+                        .ExportAndWriteLine();
+                }
+                else
+                    Console.WriteLine("\nThere are no records yet!");
+                
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void DeleteFlashcard()
+        {
+            GetFlashcards();
+            SqlConnection connection;
+
+            string flashcardId = Validation.GetString("Please type the id of the flashcaard you would like to delete or Type 0 to return to the main menu.");
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string query = $"DELETE FROM Flashcards WHERE FlashcardId = {flashcardId}";
+
+                SqlCommand command = new(query, connection);
+
+                Console.Clear();
+                if (command.ExecuteNonQuery() > 0)
+                    Console.WriteLine($"Flashcard #{flashcardId} successfully deleted!\n");
+                else
+                    Console.WriteLine($"A flashcard with id: {flashcardId} doesn't exist!\n");
+
+                connection.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void UpdateFlashcard()
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to update?");
+            Console.WriteLine("Type 1 for question.");
+            Console.WriteLine("Type 2 for answer.");
+            Console.WriteLine("Type 3 for stack name.");
+            Console.WriteLine("Type 4 for all of them.");
+            Console.WriteLine("Type any other key to return to the main menu.");
+
+            string updateChoice = Console.ReadLine();
+
+            if (updateChoice == "1")
+            {
+                GetFlashcards();
+                SqlConnection connection;
+
+                string flashcardId = Validation.GetString("Please type the id of the flashcard you would like to update or Type 0 to return to the main menu.");
+
+                string flashcardQuestion = Validation.GetString("Please type the new question or Type 0 to return to the main menu.");
+
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    string query = $"UPDATE Flashcards SET Question = ('" + flashcardQuestion + "') WHERE FlashcardId = '" + flashcardId + "'";
+
+                    SqlCommand command = new(query, connection);
+
+                    Console.Clear();
+                    if (command.ExecuteNonQuery() > 0)
+                        Console.WriteLine($"Flashcard #{flashcardId} successfully updated!\n");
+                    else
+                        Console.WriteLine($"A flashcard with id: {flashcardId} doesn't exist!\n");
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else if (updateChoice == "2")
+            {
+                GetFlashcards();
+                SqlConnection connection;
+
+                string flashcardId = Validation.GetString("Please type the id of the flashcard you would like to update or Type 0 to return to the main menu.");
+
+                string flashcardAnswer = Validation.GetString("Please type the new correct answer or Type 0 to return to the main menu.");
+
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    string query = $"UPDATE Flashcards SET Answer = ('" + flashcardAnswer + "') WHERE FlashcardId = '" + flashcardId + "'";
+
+                    SqlCommand command = new(query, connection);
+
+                    Console.Clear();
+                    if (command.ExecuteNonQuery() > 0)
+                        Console.WriteLine($"Flashcard #{flashcardId} successfully updated!\n");
+                    else
+                        Console.WriteLine($"A flashcard with id: {flashcardId} doesn't exist!\n");
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else if (updateChoice == "3")
+            {
+                GetFlashcards();
+                SqlConnection connection;
+
+                string flashcardId = Validation.GetString("Please type the id of the flashcard you would like to update or Type 0 to return to the main menu.");
+
+                Console.WriteLine();
+                GetStacks();
+                string stackId = Validation.GetString("Please type the new stack id or Type 0 to return to the main menu.");
+
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    string query = $"UPDATE Flashcards SET StackId = ('" + stackId + "') WHERE FlashcardId = '" + flashcardId + "'";
+
+                    SqlCommand command = new(query, connection);
+
+                    Console.Clear();
+                    if (command.ExecuteNonQuery() > 0)
+                        Console.WriteLine($"Flashcard #{flashcardId} successfully updated!\n");
+                    else
+                        Console.WriteLine($"A flashcard with id: {flashcardId} doesn't exist!\n");
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else if (updateChoice == "4")
+            {
+                GetFlashcards();
+                SqlConnection connection;
+
+                string flashcardId = Validation.GetString("Please type the id of the flashcard you would like to update or Type 0 to return to the main menu.");
+
+                string flashcardQuestion = Validation.GetString("Please type the new question or Type 0 to return to the main menu.");
+
+                string flashcardAnswer = Validation.GetString("Please type the new correct answer or Type 0 to return to the main menu.");
+
+                GetStacks();
+                string stackId = Validation.GetString("Please type the new stack id or Type 0 to return to the main menu.");
+
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    string query = $"UPDATE Flashcards SET " +
+                        "Question = ('" + flashcardQuestion + "'), " +
+                        "Answer = ('" + flashcardAnswer + "'), " +
+                        "StackId = ('" + stackId + "') " +
+                        "WHERE FlashcardId = '" + flashcardId + "'";
+
+                    SqlCommand command = new(query, connection);
+
+                    Console.Clear();
+                    if (command.ExecuteNonQuery() > 0)
+                        Console.WriteLine($"Flashcard #{flashcardId} successfully updated!\n");
+                    else
+                        Console.WriteLine($"A flashcard with id: {flashcardId} doesn't exist!\n");
+
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.Clear();
+                GetUserInput();
+            }
+        }
+
+        static void CheckDuplicate(string name)
+        {
+            SqlConnection connection;
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                string query = $"SELECT * FROM Stacks WHERE Name = ('"+ name +"')";
+
+                SqlCommand command = new(query, connection);
+
+                var tableData = new DataTable();
+                tableData.Load(command.ExecuteReader());
+
+                if (tableData.Rows.Count > 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"A Stack with the name {name} already exists!");
+                    GetUserInput();
+                }    
 
                 connection.Close();
 
---
+++ Flashcards/Validation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    internal class Validation
+    {
+        internal static string GetString(string message)
+        {
+            Console.WriteLine($"\n{message}");
+
+            string str = Console.ReadLine();
+
+            if (str == "")
+            {
+                Console.Clear();
+                Console.WriteLine("Wrong input. You can't insert a blank space!");
+                DatabaseManager.GetUserInput();
+            }
+
+            if (str == "0")
+            {
+                Console.Clear();
+                DatabaseManager.GetUserInput();
+            }
+
+            return str;
+        }
+    }
+}
